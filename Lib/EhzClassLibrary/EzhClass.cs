bb5353f15b589525ec3368127adc48a36db2c371
Alexey Rusakov <rusal@bk.ru>
2021-10-06 00:36:50 +0300

    Fix Library

--- Lib/EhzClassLibrary/EzhClass.cs
+++ Lib/EhzClassLibrary/EzhClass.cs
@@ -7,9 +7,9 @@ namespace EhzBattleServer.Ezh
 {
   public class EzhClass
   {
-    string name;
-    int weapon;
-    int health;
-    int damange;
+    public string name;
+    public int health;
+    public string armor;
+    public string weapon;
   }
 }
---
+++ Lib/EhzClassLibrary/InventoryClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EhzClassLibrary
+{
+  class InventoryClass
+  {
+    Dictionary<string, int> weapons = new Dictionary<string, int>();
+    Dictionary<string, int> armors = new Dictionary<string, int>();
+    Dictionary<string, int> items = new Dictionary<string, int>();
+
+    public void loadData()
+    {
+
+    }
+    public void saveData()
+    {
+
+    }
+  }
+}
